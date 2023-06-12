afd548fbf2fb2132ddc6c232e852e3153b2ee888
xKouen <ejya_10@hotmail.com>
2023-06-12 15:06:41 -0700

    13

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate --noinput  # Ejecuta las migraciones
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.create_superuser('admin', 'admin@example.com', 'password'); from myapp.models import Customer; customer = Customer.objects.create(user=user, email='admin@example.com', name='Admin Customer')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.create_superuser('admin', 'admin@example.com', 'password'); from .models import Customer; customer = Customer.objects.create(user=user, email='admin@example.com', name='Admin Customer')" | python manage.py shell
