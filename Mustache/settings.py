d1fec5b52a8a130d6cb442bc300fbb0353ba6581
xKouen <ejya_10@hotmail.com>
2023-06-11 19:49:55 -0700

    1

--- Mustache/settings.py
+++ Mustache/settings.py
@@ -124,14 +124,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = 'static/images/'
+MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
