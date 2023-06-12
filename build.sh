45e27f0cac60b11b491a203e7087f12a2230eb30
xKouen <ejya_10@hotmail.com>
2023-06-12 14:49:54 -0700

    11

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --noinput  # Ejecuta las migraciones
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
