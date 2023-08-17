825aaa14a507f00e9d128276cd0db77ef263bdad
xKouen <ejya_10@hotmail.com>
2023-08-16 22:02:19 -0700

    ´Debug mostrar imagenes

--- Mustache/settings.py
+++ Mustache/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/staticfiles/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
